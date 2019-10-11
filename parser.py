b12ac6a1c8da19530b0c13c1129ca45ca7b90c5e
张家良 <jialiang0601@gmail.com>
2019-10-11 17:33:07 -0400

    try modified left right

--- parser.py
+++ parser.py
@@ -191,7 +191,7 @@ class PartialParse(object):
                 deps.append(self.arcs[i][1])
 
         deps.sort()
-        deps = deps[0:n_to_get]
+        deps = deps[len(deps) - n_to_get:]
         # *** END YOUR CODE ***
         return deps
 
